0592b6b14ef466e320fc11477763afe8374f982d
Sagarcoolguy <115561859+Sagarcoolguy@users.noreply.github.com>
2022-10-14 11:30:48 +0530

    Add files via upload

---
+++ case.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+require_once("config.php");
+
+
+
+$ac=$_POST['tu'];
+$tc=$_POST['ta'];
+$sc=$_POST['uc'];
+$us=$_SESSION['un'];
+
+$l=strlen($ac)-5;
+$m=strlen($tc);
+
+
+
+if($l==$m)
+{	
+	$result="Accepted";
+} 
+
+else if($ac==$tc)
+{
+	$result="Accepted";
+}
+else
+{
+	$result="Wrong Answer";
+}
+
+$sql="INSERT INTO submission VALUES('','$us', '$result') ";
+$nsql="INSERT into code VALUES('us','$sc','')";
+$show="SELECT * FROM submission";
+
+$stq=mysqli_query($con,$sql);
+$send=mysqli_query($con,$nsql);
+$sts=mysqli_query($con,$show);
+
+
+
+while($row=mysqli_fetch_array($sts))
+{
+
+	echo "<div style=\"margin-left:300px; margin-right:200px; padding-left:200px;border-bottom:1px solid black;\"> <a href=\"showcode.php?id=$row[sid]&nm=$row[sname]\">$row[sid]</a>.......<a href=\"profile.php?user=$row[sname]\">$row[sname]</a>---------$row[verdict]</div>";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db="problem";
+?>
